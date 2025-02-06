0549f1c3dae17c6d0ce0ce6d1db51bdb42a27291
edsonjabastos <edsonjabastos@hotmail.com>
2025-02-06 10:34:25 -0300

    Refactor Login component to use refs for email and password inputs

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,38 +1,18 @@
-import { useState } from "react";
+import { useRef } from "react";
 
 export default function Login() {
-  // const [email, setEmail] = useState("");
-  // const [password, setPassword] = useState("");
-
-  const [loginEntry, setLoginEntry] = useState({
-    email: "",
-    password: "",
-  });
+  const email = useRef();
+  const password = useRef();
 
   function handleSubmit(event) {
     event.preventDefault();
 
-    // console.log("Email:", email);
-    // console.log("Password:", password);
+    const emailValue = email.current.value;
+    const passwordValue = password.current.value;
 
-    console.log("Email:", loginEntry.email);
-    console.log("Password:", loginEntry.password);
+    console.log("Email:", emailValue);
+    console.log("Password:", passwordValue);
   }
-
-  function handleLoginEntryChange(identifier, value) {
-    setLoginEntry({
-      ...loginEntry,
-      [identifier]: value,
-    });
-  }
-
-  // function handleEmailChange(event) {
-  //   setEmail(event.target.value);
-  // }
-
-  // function handlePasswordChange(event) {
-  //   setPassword(event.target.value);
-  // }
 
   return (
     <form onSubmit={handleSubmit}>
@@ -41,24 +21,12 @@ export default function Login() {
       <div className="control-row">
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
-          <input
-            id="email"
-            type="email"
-            name="email"
-            onChange={(e) => handleLoginEntryChange("email", e.target.value)}
-            value={loginEntry.email}
-          />
+          <input id="email" type="email" name="email" ref={email} />
         </div>
 
         <div className="control no-margin">
           <label htmlFor="password">Password</label>
-          <input
-            id="password"
-            type="password"
-            name="password"
-            onChange={(e) => handleLoginEntryChange("password", e.target.value)}
-            value={loginEntry.password}
-          />
+          <input id="password" type="password" name="password" ref={password} />
         </div>
       </div>
 
