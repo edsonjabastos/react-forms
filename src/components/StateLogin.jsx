633b1e4d24ea8b3b2829befa44e021ce06ad4010
edsonjabastos <edsonjabastos@hotmail.com>
2025-02-06 15:39:48 -0300

    Rename Login component to StateLogin for consistency

--- src/components/StateLogin.jsx
+++ src/components/StateLogin.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-export default function Login() {
+export default function StateLogin() {
   // const [email, setEmail] = useState("");
   // const [password, setPassword] = useState("");
 
