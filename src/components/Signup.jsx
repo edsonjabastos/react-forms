4c45ecca11da1c7a23a15d07083f98d5c1817252
edsonjabastos <edsonjabastos@hotmail.com>
2025-02-06 10:47:43 -0300

    Add form submission handler to Signup component

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,6 +1,16 @@
 export default function Signup() {
+  function handleSubmit(event) {
+    event.preventDefault();
+    const form = event.target;
+    const formData = new FormData(form);
+
+    const acquisitionChannel = formData.getAll("acquisition");
+    const data = Object.fromEntries(formData.entries());
+    data.acquisition = acquisitionChannel;
+    console.log(data);
+  }
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <h2>Welcome on board!</h2>
       <p>We just need a little bit of data from you to get you started 🚀</p>
 
