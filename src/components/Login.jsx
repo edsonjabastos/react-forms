ec0620cdbeceab367c523b5657daec55c46068c8
edsonjabastos <edsonjabastos@hotmail.com>
2025-02-06 10:14:58 -0300

    Refactor Login component to manage form state with useState and handle input changes

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,14 +1,39 @@
+import { useState } from "react";
+
 export default function Login() {
+  // const [email, setEmail] = useState("");
+  // const [password, setPassword] = useState("");
+
+  const [loginEntry, setLoginEntry] = useState({
+    email: "",
+    password: "",
+  });
+
   function handleSubmit(event) {
     event.preventDefault();
 
-    const form = event.target;
-    const formData = new FormData(form);
-    const data = Object.fromEntries(formData);
+    // console.log("Email:", email);
+    // console.log("Password:", password);
 
-    console.log(data);
-    console.log("Form submitted");
+    console.log("Email:", loginEntry.email);
+    console.log("Password:", loginEntry.password);
   }
+
+  function handleLoginEntryChange(identifier, value) {
+    setLoginEntry({
+      ...loginEntry,
+      [identifier]: value,
+    });
+  }
+
+  // function handleEmailChange(event) {
+  //   setEmail(event.target.value);
+  // }
+
+  // function handlePasswordChange(event) {
+  //   setPassword(event.target.value);
+  // }
+
   return (
     <form onSubmit={handleSubmit}>
       <h2>Login</h2>
@@ -16,12 +41,24 @@ export default function Login() {
       <div className="control-row">
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
-          <input id="email" type="email" name="email" />
+          <input
+            id="email"
+            type="email"
+            name="email"
+            onChange={(e) => handleLoginEntryChange("email", e.target.value)}
+            value={loginEntry.email}
+          />
         </div>
 
         <div className="control no-margin">
           <label htmlFor="password">Password</label>
-          <input id="password" type="password" name="password" />
+          <input
+            id="password"
+            type="password"
+            name="password"
+            onChange={(e) => handleLoginEntryChange("password", e.target.value)}
+            value={loginEntry.password}
+          />
         </div>
       </div>
 
